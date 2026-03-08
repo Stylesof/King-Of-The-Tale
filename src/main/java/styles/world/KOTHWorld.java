7c6e2bcc1277b9f5cc2f202f66435ea057507fa6
Styles <guidaguia1@hotmail.com>
2026-03-08 19:52:54 -0300

    First Commit

---
+++ src/main/java/styles/world/KOTHWorld.java
@@ -0,0 +1,17 @@
+package styles.world;
+
+import com.hypixel.hytale.server.core.universe.world.World;
+
+import java.awt.geom.Point2D;
+
+public class KOTHWorld {
+
+    static public Point2D generateRandomWorldPos(World world){
+
+        world.getChunkStore().getTotalGeneratedChunksCount();
+
+        return null;
+
+    }
+
+}
