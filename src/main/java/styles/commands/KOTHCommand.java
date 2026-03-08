68aa09e4cbc0908ad369b1e05dc045fee72cb850
Styles <guidaguia1@hotmail.com>
2026-03-08 19:52:54 -0300

    First Commit

---
+++ src/main/java/styles/commands/KOTHCommand.java
@@ -0,0 +1,30 @@
+package styles.commands;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.arguments.system.OptionalArg;
+import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
+import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+import static styles.utils.Utils.print;
+
+public class KOTHCommand extends AbstractCommandCollection {
+
+
+    public KOTHCommand() {
+        super("koth", "King Of The Hytale Minigame");
+
+        this.addSubCommand(new KOTHStartCommand());
+        this.addSubCommand(new KOTHStopCommand());
+    }
+
+
+}
