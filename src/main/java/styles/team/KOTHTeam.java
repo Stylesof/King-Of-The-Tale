5dca5249e2da11fda7a632b9a336cf999b9b6de5
Styles <guidaguia1@hotmail.com>
2026-03-08 21:58:41 -0300

    KOTH Commands, and others.

---
+++ src/main/java/styles/team/KOTHTeam.java
@@ -0,0 +1,17 @@
+package styles.teams;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class KOTHTeam {
+
+    private static Map<UUID, KOTHTeam> Teams = new HashMap<>();
+
+    private UUID teamID;
+
+    public KOTHTeam(UUID i){
+
+    }
+
+}
