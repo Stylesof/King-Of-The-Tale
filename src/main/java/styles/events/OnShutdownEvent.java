7f03dcdd2918b49f6b0cf9ff413d617588388ebb
Styles <guidaguia1@hotmail.com>
2026-03-12 23:53:00 -0300

    New problems

--- src/main/java/styles/events/OnShutdownEvent.java
+++ src/main/java/styles/events/OnShutdownEvent.java
@@ -1,22 +1,2 @@
-package styles.events;
-
-import com.hypixel.hytale.server.core.event.events.ShutdownEvent;
-import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
-import com.hypixel.hytale.server.core.universe.Universe;
-import com.hypixel.hytale.server.core.universe.world.World;
-
-public class OnPlayerDisconnectEvent {
-
-    public static void OnPlayerDisconnect(PlayerDisconnectEvent evt) {
-
-        World world = Universe.get().getWorld(evt.getPlayerRef().getWorldUuid());
-
-        if (world != null) {
-
-            if (world.play)
-
-        }
-
-    }
-
+package styles.events;public class OnShutdownEvent {
 }
