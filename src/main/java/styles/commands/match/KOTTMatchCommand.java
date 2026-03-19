6623e7bc941c75feba9b4f55d14c18427d4dd93a
Guilherme <guidaguia1@hotmail.com>
2026-03-19 07:11:09 -0300

    Finished world system, just need to full delete temp world

---
+++ src/main/java/styles/commands/match/KOTTMatchCommand.java
@@ -0,0 +1,10 @@
+package styles.commands.match;
+
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class KOTTMatchCommand extends AbstractCommandCollection {
+    public KOTTMatchCommand(@NonNullDecl String name, @NonNullDecl String description) {
+        super(name, description);
+    }
+}
