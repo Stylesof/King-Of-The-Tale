b278f1a517bf575338190e8b8c2a187d553cffc3
Styles <guidaguia1@hotmail.com>
2026-04-08 01:14:17 -0300

    Finisihed NPC following path to the zone... needs add random points in the zone to follow

--- src/main/java/styles/commands/test/TestCommand.java
+++ src/main/java/styles/commands/test/TestCommand.java
@@ -1,18 +1,23 @@
 package styles.commands.test;
 
 import com.hypixel.hytale.builtin.path.path.TransientPath;
+import com.hypixel.hytale.component.ComponentAccessor;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.math.vector.Vector3f;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncPlayerCommand;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.npc.INonPlayerCharacter;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.NPCPlugin;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
+import com.hypixel.hytale.server.npc.movement.controllers.ProbeMoveData;
+import com.hypixel.hytale.server.npc.navigation.*;
+import com.hypixel.hytale.server.npc.util.expression.compile.ast.AST;
 import it.unimi.dsi.fastutil.Pair;
 
 import javax.annotation.Nonnull;
@@ -49,6 +54,7 @@ public class TestCommand extends AbstractAsyncPlayerCommand {
             NPCEntity npcComponent = store.getComponent(npc.first(), NPCEntity.getComponentType());
             npcComponent.getPathManager().setTransientPath(paths);
 
+            //Path path = pathFinder.find
             /*
                 Create waypoints preset in Zone, and for every *time* reset, and wait again,
                 and finally re-set the transientPath and set to the npc
